44adab43b84b8209a05b5582d07c090ae4a5c85d
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-06 17:33:24 +0200

    [add] app context

---
+++ Patron/DAL/PatronContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+using Patron.Models;
+
+namespace Patron.DAL
+{
+    public class PatronContext : DbContext
+    {
+        public PatronContext()
+            : base("DefaultConnection")
+        {
+
+        }
+        public DbSet<Models.Patron> Patrons { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<AuthorThreshold> AuthorThresholds { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
