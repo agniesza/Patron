8f328aaa5c9daed5216b9cf10a359040b696b7d4
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-06 17:12:06 +0200

    [add] Post model

---
+++ Patron/Models/Post.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Patron.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public int AuthorID { get; set; }
+        public string Content { get; set; }
+        public double Raiting { get; set; }
+        public int NumberOfRatings { get; set; }
+
+        public virtual List<Patron> Patrons { get; set; }
+        public virtual Author Author { get; set; }
+
+
+        private void CalculateRaiting(int rate)
+        {
+            double tmp = (Raiting * NumberOfRatings) + rate;
+            NumberOfRatings++;
+            Raiting = tmp / NumberOfRatings;
+
+        }
+    }
+}
