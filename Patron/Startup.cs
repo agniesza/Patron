6960110868a157cd2016def2444be14a4799bbb0
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-06 16:46:02 +0200

    Dodaj pliki projektów.

---
+++ Patron/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Patron.Startup))]
+namespace Patron
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
