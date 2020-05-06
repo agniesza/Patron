66e268007e867836449fc4b5ef940fc4290c7686
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-06 17:11:17 +0200

    [add] Threshold model

---
+++ Patron/Models/AuthorThreshold.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Patron.Models
+{
+    public class AuthorThreshold
+    {
+        public int ID { get; set; }
+        public int AuthorID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+        public int MaxNumberOfPatrons { get; set; }
+
+        public virtual Author Author { get; set; }
+        public virtual List<Patron> Patrons { get; set; }
+    }
+}
