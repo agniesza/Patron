640024fca517bf932e1acf9a0310ac8e60017707
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-06 17:15:59 +0200

    [add] Payment model

---
+++ Patron/Models/Payment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Patron.Models
+{
+    public enum Status
+    {
+        CANCELLED,
+        ACTIVE
+    };
+    public class Payment
+    {
+        public int ID { get; set; }
+        public int AuthorID { get; set; }
+        public int PatronID { get; set; }
+        public string SourceBankAcc { get; set; }
+        public DateTime Date { get; set; }
+        public int Value { get; set; }
+        public Status Status { get; set; }
+
+        public virtual Author Author { get; set; }
+        public virtual Patron Patron { get; set; }
+
+
+
+    }
+
+}
