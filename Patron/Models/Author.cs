d78e216846a1d8a1950cc28c677fd72489943b4d
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-13 19:05:12 +0200

    [change] author model - [add] city, twitter link, change category to list of categories

--- Patron/Models/Author.cs
+++ Patron/Models/Author.cs
@@ -12,23 +12,23 @@ namespace Patron.Models
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int CategoryID { get; set; }
         public string BankAccount { get; set; }
         [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         [DataType(DataType.MultilineText)]
         public string Goals { get; set; }
+        public string City { get; set; }
 
 
 
         public string FacebookLink { get; set; }
         public string InstagramLink { get; set; }
         public string YouTubeLink { get; set; }
+        public string TwitterLink { get; set; }
         public string OtherLink { get; set; }
 
 
-        public virtual Category Category { get; set; }
-
+        public virtual List<Category> Categories { get; set; }
         public virtual List<Post> Posts { get; set; }
         public virtual List<Payment> Payments { get; set; }
         public virtual List<AuthorThreshold> AuthorThresholds { get; set; }
