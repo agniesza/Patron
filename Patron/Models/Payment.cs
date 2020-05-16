f029c8f891c023aac55a99ca957d8a696ad0f3ba
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-16 21:10:56 +0200

    [add] type in payment model

--- Patron/Models/Payment.cs
+++ Patron/Models/Payment.cs
@@ -7,6 +7,11 @@ namespace Patron.Models
         CANCELLED,
         ACTIVE
     };
+    public enum Type
+    {
+        MONTHLY,
+        ONE_TIME
+    };
     public class Payment
     {
         public int ID { get; set; }
@@ -16,6 +21,7 @@ namespace Patron.Models
         public DateTime Date { get; set; }
         public int Value { get; set; }
         public Status Status { get; set; }
+        public Type Type { get; set; }
 
         public virtual Author Author { get; set; }
         public virtual Patron Patron { get; set; }
