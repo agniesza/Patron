7b87ac612ebf98cc2e022566f37b1e4dc34eca01
agnieszka <agnieszka.gfkm@o2.pl>
2020-05-30 11:14:46 +0200

    [add] credit card model

---
+++ Patron/Models/CreditCard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Patron.Models
+{
+    public enum CardType
+    {
+        American_Express,
+        MasterCard,
+        Visa
+    };
+
+    public class CreditCard
+    {
+        public int ID { get; set; }
+        public CardType CardType { get; set; }
+        public string CardNumber { get; set; }
+        public int ExpirationMonth { get; set; }
+        public int ExpirationYear { get; set; }
+        public int CVV { get; set; }
+        public int PatronID { get; set; }
+
+        public virtual Patron Patron { get; set; }
+
+    }
+}
