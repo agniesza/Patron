745d8f4da0c68480b4ea306edcde3cecffa1454a
agnieszka <agnieszka.gfkm@o2.pl>
2020-06-14 21:28:56 +0200

    [add] payment controller

---
+++ Patron/Controllers/PaymentsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Patron.DAL;
+using Patron.Models;
+
+namespace Patron.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private PatronContext db = new PatronContext();
+
+        // GET: Payments
+        public ActionResult Index()
+        {
+            var payments = db.Payments.Include(p => p.Author).Include(p => p.Patron);
+            return View(payments.ToList());
+        }
+
+        // GET: Payments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public ActionResult Create()
+        {
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName");
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,AuthorID,PatronID,Date,Value,Status,Periodicity")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payments.Add(payment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName", payment.AuthorID);
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName", payment.PatronID);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName", payment.AuthorID);
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName", payment.PatronID);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,AuthorID,PatronID,Date,Value,Status,Periodicity")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName", payment.AuthorID);
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName", payment.PatronID);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Payment payment = db.Payments.Find(id);
+            db.Payments.Remove(payment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
