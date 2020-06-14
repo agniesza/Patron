a8fb769d6fdb29ccaff016888d50c59dc25902c5
agnieszka <agnieszka.gfkm@o2.pl>
2020-06-14 21:31:43 +0200

    [add] job trigger

---
+++ Patron/ScheduledTasks/JobScheduler.cs
@@ -0,0 +1,44 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Web;
+
+namespace Patron.ScheduledTasks
+{
+    public class JobScheduler
+    {
+         public async static void Start()
+        {
+            //   IScheduler scheduler = StdSchedulerFactory.GetDefaultScheduler();
+            NameValueCollection props = new NameValueCollection
+    {
+        { "quartz.serializer.type", "binary" }
+    };
+            StdSchedulerFactory factory = new StdSchedulerFactory(props);
+
+            // get a scheduler
+            IScheduler sched =await factory.GetScheduler();
+            sched.Start();
+
+            // define the job and tie it to our HelloJob class
+            IJobDetail job = JobBuilder.Create<PaymentJob>()
+                .WithIdentity("myJob", "group1")
+                .Build();
+
+            // Trigger the job to run now, and then every 40 seconds
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("myTrigger", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                .WithIntervalInHours(100)
+                 //   .WithIntervalInSeconds(40)
+                  .RepeatForever())
+            .Build();
+
+            await sched.ScheduleJob(job, trigger);
+        }
+    }
+}
