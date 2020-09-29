1c7f6d4aff45198e354de3f186bd3929d0a3b074
agnieszka <agnieszka.gfkm@o2.pl>
2020-09-29 17:30:52 +0200

    [add] new model (goal)

---
+++ Patron/Models/Goal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace Patron.Models
+{
+    public class Goal
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        [AllowHtml]
+        public string Description { get; set; }
+
+        public virtual Author Author { get; set; }
+
+
+    }
+}
