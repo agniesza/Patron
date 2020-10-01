256d41ff90a166ce5a26b7d4679a0a251078db35
agnieszka <agnieszka.gfkm@o2.pl>
2020-10-01 21:41:52 +0200

    [add] comments controller and views

---
+++ Patron/Controllers/CommentsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Patron.DAL;
+using Patron.Models;
+
+namespace Patron.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private PatronContext db = new PatronContext();
+
+        public ActionResult AuthorComments(int? id, int? page)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var comments = db.Comments.OrderByDescending(c => c.Date).Include(c => c.Author).Include(c => c.Patron);
+            Comment cc = comments.First();
+            ViewBag.isThisAuthor = false;
+            if (cc.Author.UserName.Equals(User.Identity.Name))
+            {
+                ViewBag.isThisAuthor = true;
+            }
+            
+
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(comments.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Comments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // GET: Comments/Create
+        public ActionResult Create()
+        {
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName");
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName");
+            return View();
+        }
+
+        // POST: Comments/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                comment.Date = DateTime.Now;
+                db.Comments.Add(comment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName", comment.AuthorID);
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName", comment.PatronID);
+            return View(comment);
+        }
+
+        // GET: Comments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName", comment.AuthorID);
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName", comment.PatronID);
+            return View(comment);
+        }
+
+        // POST: Comments/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Content,Date,Rate,PatronID,AuthorID")] Comment comment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(comment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "UserName", comment.AuthorID);
+            ViewBag.PatronID = new SelectList(db.Patrons, "ID", "UserName", comment.PatronID);
+            return View(comment);
+        }
+
+        // GET: Comments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Comment comment = db.Comments.Find(id);
+            if (comment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(comment);
+        }
+
+        // POST: Comments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Comment comment = db.Comments.Find(id);
+            db.Comments.Remove(comment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
